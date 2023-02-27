befd880c7a244ba40b2dfc046dbce187a9269e06
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-02-27 14:55:42 +0700

    add Convert string to camel case

---
+++ toCamelCase.js
@@ -0,0 +1,33 @@
+// Convert string to camel case
+
+function toCamelCase(str) {
+  let count = (str.match(/-|_/g) || []).length;
+
+  for (let i = 0; i < count; i++) {
+    let index = str.indexOf(str.match(/-|_/g)[0]);
+    let char = str.slice(index + 1, index + 2);
+    let split = str.split("");
+    split.splice(index, 2, char.toUpperCase());
+    str = split.join("");
+  }
+  return str;
+}
+
+console.log(toCamelCase(""));
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("The_Stealth_Warrior"));
+console.log(toCamelCase("A-B-C"));
+
+// Best Practice
+
+function toCamelCaseBest(str) {
+  const regExp = /[-_]\w/gi;
+  return str.replace(regExp, (match) => match.charAt(1).toUpperCase());
+
+  // return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
+}
+
+console.log(toCamelCaseBest(""));
+console.log(toCamelCaseBest("the-stealth-warrior"));
+console.log(toCamelCaseBest("The_Stealth_Warrior"));
+console.log(toCamelCaseBest("A-B-C"));
